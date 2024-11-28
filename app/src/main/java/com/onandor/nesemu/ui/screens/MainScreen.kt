c417fa5cef3a5d82077f2a5039b133448f2305ae
Nándor Oláh <olahnandi00@gmail.com>
2024-11-28 21:35:42 +0100

    Add styling to main screen

--- app/src/main/java/com/onandor/nesemu/ui/screens/MainScreen.kt
+++ app/src/main/java/com/onandor/nesemu/ui/screens/MainScreen.kt
@@ -4,7 +4,11 @@ import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -12,8 +16,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.onandor.nesemu.viewmodels.MainViewModel
 
@@ -36,11 +44,32 @@ fun MainScreen(
     val uiState by viewModel.uiState.collectAsState()
 
     Scaffold { padding ->
-        Column(modifier = Modifier.padding(padding)) {
-            Text("Rom select")
-            Button(onClick = { pickRomFileLauncher.launch(arrayOf("*/*")) }) {
-                Text("Select rom")
+        Column(
+            modifier = Modifier
+                .padding(padding)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Text(
+                modifier = Modifier.padding(top = 200.dp),
+                text = "NES Emulator",
+                fontWeight = FontWeight.Bold,
+                fontSize = 40.sp
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            Button(
+                modifier = Modifier
+                    .width(250.dp)
+                    .height(75.dp),
+                onClick = { pickRomFileLauncher.launch(arrayOf("*/*")) }
+            ) {
+                Text(
+                    text = "Select rom",
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 20.sp
+                )
             }
+            Spacer(modifier = Modifier.height(200.dp))
         }
     }
 
